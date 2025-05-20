e8faab5c449ae881085103dec1f06d817aae577f
Huy04-TB <huynqhe182181@fpt.edu.vn>
2025-05-20 14:53:10 +0700

    abc

---
+++ MaterialManagement/src/java/dal/DBConnect.java
@@ -0,0 +1,26 @@
+package dal;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnect {
+    protected Connection connection;
+    public DBConnect() {
+        try {
+            String url = "jdbc:mysql://127.0.0.1:3306/beo?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+            String username = "root";
+            String password = "1234";
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(url, username, password);
+            System.out.println("✅ Kết nối thành công tới MySQL!");
+
+        } catch (ClassNotFoundException | SQLException ex) {
+            System.out.println("❌ Lỗi kết nối: " + ex.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        new DBConnect();
+    }
+}
